db06c2b73cc230d0e49183271f8f14d2673b4f91
timstasevich <tim.stasevich@colostate.edu>
2026-04-07 19:59:24 -0600

    Fix pip install: add scripts to PATH and example_data inside package

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     scripts=["scripts/run_scfvtools_scoring.py"],
+    package_data={"scfvtools": ["example_data/*"]},
+    include_package_data=True,
 )
